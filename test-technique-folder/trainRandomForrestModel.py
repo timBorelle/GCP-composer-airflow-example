d5c68dd222338e39a661c0881217f54543d7c6ef
timBorelle <tim-borelle@protonmail.com>
2022-12-16 20:20:31 +0100

    update model

--- test-technique-folder/trainRandomForrestModel.py
+++ test-technique-folder/trainRandomForrestModel.py
@@ -1,55 +1,50 @@
 import pandas as pd
-
-# from sklearn import datasets
-#from datasets import load_dataset
+import joblib
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
 #from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
-
-# gs://test-technique-folder-tim/data/train.csv
+#df = pd.read_csv("gs://test-technique-folder-tim/data/train.csv", sep=";")
 # gs://test-technique-folder-tim/data/test.csv
-#dataset = load_dataset('csv', data_files={'train': ['data/data_train.csv'],
-#                                          'test': 'data/data_test.csv'})
 df = pd.read_csv("data/data_train.csv", sep=";")
-df.sample(5, random_state=44)
-
+#df.sample(5, random_state=44)
 
 df = df.dropna()
 
-#train = dataset['train']        # .drop(['quality'], axis=1)
-#test = dataset['test']
-
 # Separating the features (X) and the Labels (y)
-X = df.drop(["fixed acidity",
-    "volatile acidity",
-    "citric acid",
-    "residual sugar",
-    "chlorides",
-    "free sulfur dioxide",
-    "total sulfur dioxide",
-    "density",
-    "pH",
-    "sulphates",
-    "alcohol"], axis=1)
+X = df.drop(["quality"], axis=1)
+#X = df.drop(["fixed acidity",
+#    "volatile acidity",
+#    "citric acid",
+#    "residual sugar",
+#    "chlorides",
+#    "free sulfur dioxide",
+#    "total sulfur dioxide",
+#    "density",
+#    "pH",
+#    "sulphates",
+#    "alcohol"], axis=1)
 y = df["quality"]
 
-print(X.head(5))
-#print(train)
+#print(X.head(5))
+#print(y.head(5))
 
 # Training our random forest model
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=44)
-## define the model
-#model = RandomForestRegressor()
-rf_model = RandomForestClassifier(n_estimators=50, max_features="sqrt", random_state=44)
-rf_model.fit(X_train, y_train)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=44)
+# define the pipeline
+pipeline = Pipeline([('normalize', StandardScaler()),
+                     ('regressor', RandomForestRegressor())])
 
-# fit the model on the whole dataset
-#model.fit(X, y)
+# Fit the model
+pipeline.fit(X, y)
 
 # Making predictions with our model
-predictions = rf_model.predict(X_test)
-print(predictions)
+#predictions = rf_model.predict(X_test)
+#print(predictions)
+
+# Save the model
+joblib.dump(pipeline, 'my_model.joblib')
 
